d2a8d9b863a3b11cf45124f4d981fc83f3fe9cc1
Carissa Redfield <cred@sas.upenn.edu>
2015-12-18 14:51:15 -0500

    updated phonemes with place and voice

--- src/phonemes/PHONEME.java
+++ src/phonemes/PHONEME.java
@@ -8,127 +8,124 @@ package phonemes;
  */
 
 public enum PHONEME {
+    
+ // VOWELS
 
-	AO (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
-	AA (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
-	IY (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
-	UW (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
-	EH (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
-	IH (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
-	UH (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
-	AH (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
-	AX (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
-	AE (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
-	
+ AO (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
+ AA (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
+ IY (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
+ UW (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
+ EH (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
+ IH (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
+ UH (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
+ AH (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
+ AX (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
+ AE (GROUP.VOWEL, MANNER.MONOPHTHONG, null, null),
+ 
 
-	EY (GROUP.VOWEL, MANNER.DIPHTHONG, null, null),
-	AY (GROUP.VOWEL, MANNER.DIPHTHONG, null, null),
-	OW (GROUP.VOWEL, MANNER.DIPHTHONG, null, null),
-	AW (GROUP.VOWEL, MANNER.DIPHTHONG, null, null),
-	OY (GROUP.VOWEL, MANNER.DIPHTHONG, null, null),
-	
+ EY (GROUP.VOWEL, MANNER.DIPHTHONG, null, null),
+ AY (GROUP.VOWEL, MANNER.DIPHTHONG, null, null),
+ OW (GROUP.VOWEL, MANNER.DIPHTHONG, null, null),
+ AW (GROUP.VOWEL, MANNER.DIPHTHONG, null, null),
+ OY (GROUP.VOWEL, MANNER.DIPHTHONG, null, null),
+ 
 
-	ER (GROUP.VOWEL, MANNER.RCOLORED, null, null),
-	AXR (GROUP.VOWEL, MANNER.RCOLORED, null, null),
-	EH_R (GROUP.VOWEL, MANNER.RCOLORED, null, null),
-	AO_R (GROUP.VOWEL, MANNER.RCOLORED, null, null),
-	AA_R (GROUP.VOWEL, MANNER.RCOLORED, null, null),
-	IH_R (GROUP.VOWEL, MANNER.RCOLORED, null, null),
-	IY_R (GROUP.VOWEL, MANNER.RCOLORED, null, null),
-	AW_R (GROUP.VOWEL, MANNER.RCOLORED, null, null),
-	
+ ER (GROUP.VOWEL, MANNER.RCOLORED, null, null),
+ AXR (GROUP.VOWEL, MANNER.RCOLORED, null, null),
+ EH_R (GROUP.VOWEL, MANNER.RCOLORED, null, null),
+ AO_R (GROUP.VOWEL, MANNER.RCOLORED, null, null),
+ AA_R (GROUP.VOWEL, MANNER.RCOLORED, null, null),
+ IH_R (GROUP.VOWEL, MANNER.RCOLORED, null, null),
+ IY_R (GROUP.VOWEL, MANNER.RCOLORED, null, null),
+ AW_R (GROUP.VOWEL, MANNER.RCOLORED, null, null),
+ 
+ // CONSONANTS
 
-	P (GROUP.CONSONANT, MANNER.STOP, null, null),
-	B (GROUP.CONSONANT, MANNER.STOP, null, null),
-	T (GROUP.CONSONANT, MANNER.STOP, null, null),
-	D (GROUP.CONSONANT, MANNER.STOP, null, null),
-	K (GROUP.CONSONANT, MANNER.STOP, null, null),
-	G (GROUP.CONSONANT, MANNER.STOP, null, null),
-	
-	
-	CH (GROUP.CONSONANT, MANNER.AFFRICATE, null, null),
-	JH (GROUP.CONSONANT, MANNER.AFFRICATE, null, null),
-	
-	
-	F (GROUP.CONSONANT, MANNER.FRICATIVE, null, null),
-	V (GROUP.CONSONANT, MANNER.FRICATIVE, null, null),
-	TH (GROUP.CONSONANT, MANNER.FRICATIVE, null, null),
-	DH (GROUP.CONSONANT, MANNER.FRICATIVE, null, null),
-	S (GROUP.CONSONANT, MANNER.FRICATIVE, null, null),
-	Z (GROUP.CONSONANT, MANNER.FRICATIVE, null, null),
-	SH (GROUP.CONSONANT, MANNER.FRICATIVE, null, null),
-	ZH (GROUP.CONSONANT, MANNER.FRICATIVE, null, null),
-	HH (GROUP.CONSONANT, MANNER.FRICATIVE, null, null),
-	
+ P (GROUP.CONSONANT, MANNER.STOP, PLACE.BILABIAL, VOICE.VOICELESS),
+ B (GROUP.CONSONANT, MANNER.STOP, PLACE.BILABIAL, VOICE.VOICED),
+ T (GROUP.CONSONANT, MANNER.STOP, PLACE.ALVEOLAR, VOICE.VOICELESS),
+ D (GROUP.CONSONANT, MANNER.STOP, PLACE.ALVEOLAR, VOICE.VOICED),
+ K (GROUP.CONSONANT, MANNER.STOP, PLACE.VELAR, VOICE.VOICELESS),
+ G (GROUP.CONSONANT, MANNER.STOP, PLACE.VELAR, VOICE.VOICED),
+ 
+ 
+ CH (GROUP.CONSONANT, MANNER.AFFRICATE, PLACE.POSTALVEOLAR, VOICE.VOICELESS),
+ JH (GROUP.CONSONANT, MANNER.AFFRICATE, PLACE.POSTALVEOLAR, VOICE.VOICED),
+ 
+ 
+ F (GROUP.CONSONANT, MANNER.FRICATIVE, PLACE.LABIODENTAL, VOICE.VOICELESS),
+ V (GROUP.CONSONANT, MANNER.FRICATIVE, PLACE.LABIODENTAL, VOICE.VOICED),
+ TH (GROUP.CONSONANT, MANNER.FRICATIVE, PLACE.DENTAL, VOICE.VOICELESS),
+ DH (GROUP.CONSONANT, MANNER.FRICATIVE, PLACE.DENTAL, VOICE.VOICED),
+ S (GROUP.CONSONANT, MANNER.FRICATIVE, PLACE.ALVEOLAR, VOICE.VOICELESS),
+ Z (GROUP.CONSONANT, MANNER.FRICATIVE, PLACE.ALVEOLAR, VOICE.VOICED),
+ SH (GROUP.CONSONANT, MANNER.FRICATIVE, PLACE.POSTALVEOLAR, VOICE.VOICELESS),
+ ZH (GROUP.CONSONANT, MANNER.FRICATIVE, PLACE.POSTALVEOLAR, VOICE.VOICED),
+ HH (GROUP.CONSONANT, MANNER.FRICATIVE, PLACE.UVULAR, VOICE.VOICELESS),
+ 
 
-	M (GROUP.CONSONANT, MANNER.NASAL, null, null),
-	EM (GROUP.CONSONANT, MANNER.NASAL, null, null),
-	N (GROUP.CONSONANT, MANNER.NASAL, null, null),
-	EN (GROUP.CONSONANT, MANNER.NASAL, null, null),
-	NG (GROUP.CONSONANT, MANNER.NASAL, null, null),
-	ENG (GROUP.CONSONANT, MANNER.NASAL, null, null),
-	
+ M (GROUP.CONSONANT, MANNER.NASAL, PLACE.BILABIAL, VOICE.VOICED),
+ N (GROUP.CONSONANT, MANNER.NASAL, PLACE.ALVEOLAR, VOICE.VOICED),
+ NG (GROUP.CONSONANT, MANNER.NASAL, PLACE.VELAR, VOICE.VOICED),
 
-	L (GROUP.CONSONANT, MANNER.APPROXIMANT, null, null),
-	R (GROUP.CONSONANT, MANNER.APPROXIMANT, null, null),
-	DX (GROUP.CONSONANT, MANNER.APPROXIMANT, null, null),
-	NX (GROUP.CONSONANT, MANNER.APPROXIMANT, null, null),
-	
+ L (GROUP.CONSONANT, MANNER.LATERALAPPROXIMANT, PLACE.ALVEOLAR, VOICE.VOICED),
+ R (GROUP.CONSONANT, MANNER.APPROXIMANT, PLACE.ALVEOLAR, VOICE.VOICED),
+ DX (GROUP.CONSONANT, MANNER.FLAP, PLACE.ALVEOLAR, VOICE.VOICED),
+ 
 
-	Y (GROUP.CONSONANT, MANNER.SEMIVOWEL, null, null),
-	W (GROUP.CONSONANT, MANNER.SEMIVOWEL, null, null),
-	Q (GROUP.CONSONANT, MANNER.SEMIVOWEL, null, null);
-	
-	private GROUP group;
-	private MANNER manner;
-	private PLACE place;
-	private VOICE voice;
-	
-	PHONEME(GROUP group, MANNER manner, PLACE place, VOICE voice) {
-		this.group = group;
-		this.manner = manner;
-		this.place = place;
-		this.voice = voice;
-	}
-	
-	public GROUP getGroup() {
-		return group;
-	}
+ Y (GROUP.CONSONANT, MANNER.APPROXIMANT, PLACE.PALATAL, VOICE.VOICED),
+ W (GROUP.CONSONANT, MANNER.APPROXIMANT, PLACE.VELAR, VOICE.VOICELESS);
+ 
+ private GROUP group;
+ private MANNER manner;
+ private PLACE place;
+ private VOICE voice;
+ 
+ PHONEME(GROUP group, MANNER manner, PLACE place, VOICE voice) {
+  this.group = group;
+  this.manner = manner;
+  this.place = place;
+  this.voice = voice;
+ }
+ 
+ public GROUP getGroup() {
+  return group;
+ }
 
-	public void setGroup(GROUP group) {
-		this.group = group;
-	}
+ public void setGroup(GROUP group) {
+  this.group = group;
+ }
 
-	public MANNER getManner() {
-		return manner;
-	}
+ public MANNER getManner() {
+  return manner;
+ }
 
-	public void setManner(MANNER manner) {
-		this.manner = manner;
-	}
+ public void setManner(MANNER manner) {
+  this.manner = manner;
+ }
 
-	public PLACE getPlace() {
-		return place;
-	}
+ public PLACE getPlace() {
+  return place;
+ }
 
-	public void setPlace(PLACE place) {
-		this.place = place;
-	}
+ public void setPlace(PLACE place) {
+  this.place = place;
+ }
 
-	public VOICE getVoice() {
-		return voice;
-	}
+ public VOICE getVoice() {
+  return voice;
+ }
 
-	public void setVoice(VOICE voice) {
-		this.voice = voice;
-	}
+ public void setVoice(VOICE voice) {
+  this.voice = voice;
+ }
 
-	@Override
-	public String toString() {
-		String str = "";
-		str += name() + ": " + group + ", " + manner + ", " + place + ", " + voice;
-		return str;
-	}
-	
-	
+ @Override
+ public String toString() {
+  String str = "";
+  str += name() + ": " + group + ", " + manner + ", " + place + ", " + voice;
+  return str;
+ }
+ 
+ 
 }
--- src/phonemes/PLACE.java
+++ src/phonemes/PLACE.java
@@ -3,13 +3,14 @@ package phonemes;
  * Place that the phoneme occurs
  */
 public enum PLACE {
-	BILABIAL,
-	LABIODENTAL,
-	DENTAL,
-	ALVEOLAR,
-	POSTALVEOLAR,
-	RETROFLEX,
-	VELAR,
-	UVULAR,
-	GLOTTAL;
+ BILABIAL,
+ LABIODENTAL,
+ DENTAL,
+ ALVEOLAR,
+ POSTALVEOLAR,
+ RETROFLEX,
+ VELAR,
+ UVULAR,
+ PALATAL,
+ GLOTTAL;
 }
